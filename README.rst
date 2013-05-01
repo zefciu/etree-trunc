ad6622a0358774fc3b6315ab70ff37da8692307b
Szymon Pyżalski <szymon@pythonista.net>
2013-05-01 16:31:59 +0200

    README

---
+++ README.rst
@@ -0,0 +1,2 @@
+A simple way to truncate the content of an HTML etree. Currently very naive -
+leaves all the tree structure and clears only the text and tails.
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
       name='etree-trunc',
       version='0.1',
       author = 'Szymon Pyżalski',
       author_email = 'zefciu <szymon@pythonista.net>',
       description = 'Truncates an etree',
+      long_description = long_description,
       keywords = 'html truncate',
       packages=find_packages(),
       classifiers = [
